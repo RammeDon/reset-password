e11d4da2bb648b571d4aa36fbcd50e35bad86a84
RammeDon <raminkhareji@gmail.com>
2023-01-10 18:03:12 +0100

    initial build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [email, setEmail] = useState("")
+
+  const handleOnChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Enter your email here</p>
+      <input value={email} type="text" className='email-input' placeholder='enter your email here...' onChange={handleOnChange}></input>
+      <div>
+        <button >Send token</button>
+      </div>
+      
     </div>
   );
 }
